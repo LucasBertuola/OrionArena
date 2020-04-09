14f43c558b118688ce0f05431c2974773a6a6de0
Gabriel Conti <ga_con@hotmail.com>
2020-04-09 16:45:16 -0300

    Adcionado sons de tiro e dos botaos

---
+++ OrionArena/Assets/SfxButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SfxButton : MonoBehaviour
+{
+    public AudioClip[] clips;
+    private AudioSource audio;
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+    }
+
+    public void PlaySound()
+    {
+        audio.clip = clips[Random.Range(0, clips.Length)];
+        audio.Play();
+    }
+}
